5581512ead1262f3d378e9782b17abda6a520582
mohafizmemon0118-spec <mohafizmemon0118@gmail.com>
2026-03-01 11:58:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,22 @@
-const CACHE_NAME = "obgyn-os-v3";
+const CACHE_NAME = "obgyn-os-v4";
 
-const urlsToCache = [
-  "/obgyn-academic-os/",
-  "/obgyn-academic-os/index.html",
-  "/obgyn-academic-os/style.css"
-];
-
-// Install
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
 });
 
-// Activate (Delete old caches)
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then(response => response)
+      .catch(() => caches.match(event.request))
   );
 });
